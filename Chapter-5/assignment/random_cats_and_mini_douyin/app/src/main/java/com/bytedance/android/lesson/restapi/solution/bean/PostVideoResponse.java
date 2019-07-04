fc013e875ebce5130847a8c4eb51faeba9a913b7
KarCute <792184748@qq.com>
2019-07-04 21:57:39 +0800

    homework-5 finished

---
+++ Chapter-5/assignment/random_cats_and_mini_douyin/app/src/main/java/com/bytedance/android/lesson/restapi/solution/bean/PostVideoResponse.java
@@ -0,0 +1,31 @@
+package com.bytedance.android.lesson.restapi.solution.bean;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author Xavier.S
+ * @date 2019.01.18 17:53
+ */
+public class PostVideoResponse {
+
+    // TODO-C2 (3) Implement your PostVideoResponse Bean here according to the response json
+
+//    @SerializedName("success") private boolean success;
+//    @SerializedName("item") private Feed item;
+//
+//    public boolean getSuccess() { return success; }
+//
+//    public void setSuccess(boolean success){ this.success = success; }
+//
+//    public Feed getItem(){ return item; }
+//
+//    public void setItem(Feed item){ this.item = item; }
+    private Object result;
+    private String url;
+    private boolean success;
+
+    public Object getResult(){return result;}
+    public String getUrl(){return url;}
+    public boolean getSuccess(){return success;}
+
+}
